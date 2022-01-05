ab85d41fcec737c073dd707f6a22aa3819fafe6c
Tyler Simoni <tyler.simoni.8@gmail.com>
2022-01-05 15:33:29 -0500

    Adding test recipe data for testing

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -8,7 +8,7 @@ const Sidebar = ({ type }) => {
   };
 
   return (
-    <aside className='w-[220px] py-4 px-4 bg-amber-200 overflow-hidden'>
+    <aside className='w-[300px] py-4 px-4 bg-amber-200 overflow-hidden drop-shadow-[5px_0_5px_rgba(0,0,0,0.25)] drop '>
       <div className='flex border-b-2 border-black px-1 py-2 justify-between'>
         <h1 className='text-xl '>{type}</h1>
         <button>
